a09473283129dbbb7d1e927f7b1c63e922ed6206
KarisAya <karisaya@foxmail.com>
2026-04-19 03:26:20 +0800

    升级

--- clovers-agent/clovers_agent/main.py
+++ clovers-agent/clovers_agent/main.py
@@ -3,6 +3,7 @@ if not __package__:
 
 import httpx
 from clovers import Plugin, Result
+from clovers_client import Event as EventProtocol
 from .core import Event, CloversAgent
 from .config import Config
 
@@ -11,7 +12,7 @@ ASYNC_CLIENT = httpx.AsyncClient(timeout=300)
 AGENT = CloversAgent("CloversAgent", ASYNC_CLIENT, Config.sync_config(__package__))
 
 PLUGIN = Plugin[Event](priority=100)
-PLUGIN.protocol = Event
+PLUGIN.protocol = EventProtocol
 
 PLUGIN.startup(AGENT.init)
 PLUGIN.shutdown(ASYNC_CLIENT.aclose)
